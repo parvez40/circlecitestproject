c733fa673f635eb38619625c853abe44f5c2a52a
pahmedxain <parvez.ahmed@xain-group.com>
2018-03-08 00:06:54 +0100

    first

---
+++ truffle.js
@@ -0,0 +1,21 @@
+module.exports = {
+  build: {
+    "index.html": "index.html",
+    "app.js": [
+      "javascripts/app.js"
+    ],
+    "app.css": [
+      "stylesheets/app.css"
+    ],
+    "images/": "images/"
+  },
+  rpc: {
+    host: "localhost",
+    port: 8545
+  },
+  networks:{
+    "production": {
+      "network_id": 20170123
+    }
+  }
+};
